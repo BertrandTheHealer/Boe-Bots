91179a962e4c75f2cf1e8f2c47ef683e294b072f
MEDavy <maximiliandavy@gmail.com>
2018-03-05 10:41:44 -0600

    Analog Voltage

--- DisplayBothLineSensors/DisplayBothLineSensors.ino
+++ DisplayBothLineSensors/DisplayBothLineSensors.ino
@@ -1,17 +1,15 @@
 void setup() {
   // put your setup code here, to run once:
-  pinMode(4, INPUT);//right line sensor
-  pinMode(5, INPUT);//left line sensor
-  tone(9, 3000, 1000);// Play tone for 1 second
-  delay(1000);// Delay to finish tone
+  pinMode(10, INPUT);//right line sensor
+  pinMode(9, INPUT);//left line sensor
   Serial.begin(9600);// Set data rate to 9600 bps
 }
 
 void loop() {
 
-  Serial.print(digitalRead(5));                      // Display left distance
+  //Serial.print(analogRead(9));                      // Display left distance
   Serial.print("  ");                        // Display spaces
-  Serial.println(digitalRead(4));                   // Display right distance
+  Serial.println(digitalRead(10));                   // Display right distance
 
   delay(100);                                // 0.1 second delay
 }
