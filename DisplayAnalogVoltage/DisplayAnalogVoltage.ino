c3f29347ebc83e726471ff23f64592a1f3ce29ab
MEDavy <maximiliandavy@gmail.com>
2018-03-21 22:19:54 -0500

    Line follow

--- DisplayAnalogVoltage/DisplayAnalogVoltage.ino
+++ DisplayAnalogVoltage/DisplayAnalogVoltage.ino
@@ -1,11 +1,12 @@
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
+  pinMode(4, INPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  int sensorValue = digitalRead(11);
+  int sensorValue = digitalRead(4);
   //float voltage= sensorValue * (5.0 / 1023.0);
   Serial.println(sensorValue);
 }
